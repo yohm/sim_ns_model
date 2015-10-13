ceea5348b4023b1621c5782344953f737f27f32e
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-10-13 19:00:22 +0900

    fixed a bug in shell script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -eux
+#!/bin/bash -ex
 
 THIS_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 echo $THIS_DIR
-BUNDLE_GEMFILE=script_dir=${THIS_DIR}/Gemfile
+export BUNDLE_GEMFILE=${THIS_DIR}/Gemfile
 bundle exec ruby ${THIS_DIR}/nagel_schreckenberg.rb $@
